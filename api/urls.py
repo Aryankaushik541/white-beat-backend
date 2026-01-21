0be2dc7a9cae22a8ef77db952e1f9b092e2e3e58
Aryan kaushik <Aryankaushik541@gmail.com>
2026-01-22 02:28:44 +0530

    Add verify-admin endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('verify-admin/', views.verify_admin, name='verify_admin'),
     path('chat/', views.chat, name='chat'),
     path('admin/stats/', views.admin_stats, name='admin_stats'),
     path('health/', views.health_check, name='health_check'),
